3680a3279c38daddd9672b6591109f67be77bf15
Shafayet Fahim <s.fahim@rochester.edu>
2025-10-30 13:35:59 -0400

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 3 ms (90.5%), Space: 21.2 MB (61.86%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,20 +1,8 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freqMap = {}
-        topK = []
         for i in range(len(nums)):
             if nums[i] in freqMap: freqMap[nums[i]] += 1
             else: freqMap[nums[i]] = 1
         
-
-        # My initial solution.
-        # j = 1        
-        # while j <= k:
-        #     kth = max(freqMap, key=freqMap.get)
-        #     topK.append(kth)
-        #     del freqMap[kth]
-        #     j += 1
-        # return topK
-
-        # List comprehension and lambda functions for sorting.
-        return [num for num, _ in sorted(freqMap.items(), key=lambda x: x[1], reverse=True)[:k]]
+        return [num for num, count in sorted(freqMap.items(), key=lambda x: x[1], reverse=True)[:k]]
