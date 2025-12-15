82a1f50d1ede59af8f2cec51ad22290553b1ce28
Shafayet Fahim <s.fahim@rochester.edu>
2025-12-15 00:23:01 -0500

    0412-FIZZ-BUZZ Stats: Time: 0 ms (100%), Space: 18.5 MB (25.78%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -2,10 +2,8 @@ class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         answer = [""]*n
         for i in range(1, n+1):
-            string = ""
-            if i%3 == 0: string += "Fizz"
-            if i%5 == 0: string += "Buzz"
-            if string == "": string += str(i)
-            answer[i-1] = string
+            if i%3 == 0: answer[i-1] += "Fizz"
+            if i%5 == 0: answer[i-1] += "Buzz"
+            if answer[i-1] == "": answer[i-1] += str(i)
         return answer
         
