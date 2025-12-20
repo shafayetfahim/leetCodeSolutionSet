55188ebbdbdfde6d7f2c58f9fffa8f12629a245a
Shafayet Fahim <s.fahim@rochester.edu>
2025-12-19 21:20:22 -0500

    0001-TWO-SUM Stats: Time: 1 ms (56.53%), Space: 18.5 MB (79.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,8 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}
-        for i, n in enumerate(nums):
-            if (target - n) in prevMap: return [prevMap[(target - n)], i]
-            prevMap[n] = i
+    def twoSum(self, nums: List[int], target: int) -> List[int]:        
+        visited = {}
+        for index, value in enumerate(nums):
+            goal = target - value
+            if goal in visited: return [visited[goal], index]
+            else: visited[value] = index
+
