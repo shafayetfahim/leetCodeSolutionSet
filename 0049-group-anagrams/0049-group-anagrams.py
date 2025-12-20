908dcb0c5d2b23897466dc63e110d0cd4c991ef5
Shafayet Fahim <s.fahim@rochester.edu>
2025-12-19 21:52:48 -0500

    0049-GROUP-ANAGRAMS Stats: Time: 19 ms (37.11%), Space: 21.8 MB (36.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,6 +3,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = defaultdict(list)
         for string in strs:
-            canonical = tuple(sorted(string))
-            groups[canonical].append(string)
+            canonical = [0] * 26 
+            for char in string: canonical[ord(char) - ord('a')] += 1
+            groups[tuple(canonical)].append(string)
         return list(groups.values())
