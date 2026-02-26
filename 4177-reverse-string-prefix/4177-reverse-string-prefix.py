a01c1cc826c47f72148c8c7b92bbd9cc993abd37
Shafayet Fahim <s.fahim@rochester.edu>
2026-02-25 23:04:17 -0500

    4177-REVERSE-STRING-PREFIX Stats: Time: 0 ms (100%), Space: 19.4 MB (6.3%) - LeetHub

--- 4177-reverse-string-prefix/4177-reverse-string-prefix.py
+++ 4177-reverse-string-prefix/4177-reverse-string-prefix.py
@@ -1,7 +1,4 @@
 class Solution:
     def reversePrefix(self, s: str, k: int) -> str:
-        if len(s) == 1 or k == 1: return s
-        if len(s) == k: return s[::-1]
-
         return s[:k][::-1] + s[k::]
         
